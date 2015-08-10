101981b266b89be1f7852e2a21e03d7d72a11235
Mathias Caldas <mathiascaldas@gmail.com>
2015-08-10 19:30:37 +0300

    Changed sender API Key

--- push-android/push.js
+++ push-android/push.js
@@ -14,7 +14,7 @@ var message = new gcm.Message({
 
 var regIds = ['APA91bF5kaGRQEX8VXJPTVkdUkXwrlKdUJUhz9zb7XukN7zfb7QarOlUKQb91QI0o9gACbmmy9z0Bnod4gL4yi9gcGb8Pj4KJP19HNUaB-VHMcMDSKL9HjBz4QZCCxoUhvE9-JTRKZro'];
 
-var sender = new gcm.Sender('208976847098');
+var sender = new gcm.Sender('AIzaSyDl2ljeiVBwBg85BkBFLhZ3551v-HRXjCQ');
 
 sender.send(message, regIds, function (err, result) {
     if(err) console.error(err);
