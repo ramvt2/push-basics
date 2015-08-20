bba648fbaf8612f96f734948210e1adee687b60c
Lior Rabin <lior.rabin@scrnz.com>
2015-08-20 12:09:12 +0300

    [+} sender API keys for all apps

--- push-android/push.js
+++ push-android/push.js
@@ -1,5 +1,9 @@
 var gcm = require('node-gcm');
 
+var FABLife = 'AIzaSyD3TF8eBjT5XtXUZNeJgFJDPc2IIlpTJ94';
+var Conecta = 'AIzaSyDl2ljeiVBwBg85BkBFLhZ3551v-HRXjCQ';
+var Uninovelas = 'AIzaSyA2TY3bxG5oiFIq2qhBVgMZNmQoQeEkQNc';
+
 var message = new gcm.Message({
     timeToLive: 24*3600,
     data: {
@@ -14,7 +18,7 @@ var message = new gcm.Message({
 
 var regIds = ['APA91bF5kaGRQEX8VXJPTVkdUkXwrlKdUJUhz9zb7XukN7zfb7QarOlUKQb91QI0o9gACbmmy9z0Bnod4gL4yi9gcGb8Pj4KJP19HNUaB-VHMcMDSKL9HjBz4QZCCxoUhvE9-JTRKZro'];
 
-var sender = new gcm.Sender('AIzaSyDl2ljeiVBwBg85BkBFLhZ3551v-HRXjCQ');
+var sender = new gcm.Sender(Conecta);
 
 sender.send(message, regIds, function (err, result) {
     if(err) console.error(err);
