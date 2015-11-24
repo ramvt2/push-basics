99809340b9cc2960dada995bf0a33a257b2e3afc
Lior Rabin <lior.rabin@scrnz.com>
2015-11-24 17:02:18 +0200

    [*] minor stuff

--- push-ios/push.js
+++ push-ios/push.js
@@ -1,6 +1,6 @@
 var apn = require('apn');
 var isProd = true;
-var project = 'fablife/enterprise';
+var project = 'conecta/enterprise';
 var options = {
 	 'cert': './certs/'+ project + '/' + (isProd ? 'prod' : 'dev') + '/cert.pem',
 	 'key': './certs/'+ project + '/' + (isProd ? 'prod' : 'dev') + '/key.pem',
@@ -9,7 +9,7 @@ var options = {
 };
 var apnConnection = new apn.Connection(options);
 
-var token = 'a7fed2e95fdc11570cccbac4a46c29f4bd28ef93779ca7e49044628698727632';
+var token = '9df37608c0854888597685437c5f005c9484e784960780c35c2d061f78bfab92';
 var myDevice = new apn.Device(token);
 
 var note = new apn.Notification();
@@ -29,8 +29,16 @@ note.contentAvailable = true;
 
 apnConnection.pushNotification(note, myDevice);
 
+apnConnection.on('error', function(data){
+ console.log('error', data);
+});
+
 apnConnection.on('transmissionError', function(data){
  console.log('transmissionError', data);
+});
+
+apnConnection.on('socketError', function(data){
+ console.log('socketError', data);
 });
 
 apnConnection.on('transmitted', function(data){
